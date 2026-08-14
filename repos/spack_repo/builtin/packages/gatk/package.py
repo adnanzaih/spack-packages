b2414f3590f05657433d04a4ab5e4a3141c83950
adnan-umich <adnanzai@umich.edu>
2026-08-13 22:42:04 -0400

    committing gatk, mothur

--- repos/spack_repo/builtin/packages/gatk/package.py
+++ repos/spack_repo/builtin/packages/gatk/package.py
@@ -66,7 +66,7 @@ class Gatk(Package):
     # output.
     variant("r", default=False, description="Use R for plotting")
 
-    depends_on("java@17", type="run", when="@4.4:")
+    depends_on("java@17:", type="run", when="@4.4:")
     depends_on("java@8", type="run", when="@:4.3")
     depends_on("python@2.6:2.8,3.6:", type="run", when="@4.0:")
     depends_on("r@3.2:", type="run", when="@4.0: +r")
