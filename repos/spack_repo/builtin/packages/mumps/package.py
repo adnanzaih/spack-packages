127d8cebf33d61459182f36ce5a39e36d115d6ee
adnan-umich <adnanzai@umich.edu>
2026-08-24 14:42:59 -0400

    aocc stack updates for mumps and python

--- repos/spack_repo/builtin/packages/mumps/package.py
+++ repos/spack_repo/builtin/packages/mumps/package.py
@@ -184,6 +184,7 @@ class Mumps(Package):
 
         # Determine which compiler suite we are using
         using_gcc = self.compiler.name == "gcc"
+        using_aocc = self.compiler.name == "aocc"
         using_nvhpc = self.compiler.name == "nvhpc"
         using_intel = self.compiler.name in ("intel", "intel-oneapi-compilers-classic")
         using_oneapi = self.compiler.name in ("oneapi", "intel-oneapi-compilers")
@@ -305,8 +306,8 @@ class Mumps(Package):
 
         # TODO: change the value to the correct one according to the
         # compiler possible values are -DAdd_, -DAdd__ and/or -DUPPER
-        if using_intel or using_oneapi or using_nvhpc or using_fj:
-            # Intel, PGI, and Fujitsu Fortran compiler provides
+        if using_aocc or using_intel or using_oneapi or using_nvhpc or using_fj:
+            # AOCC, Intel, PGI, and Fujitsu Fortran compilers provide
             # the main() function so C examples linked with the Fortran
             # compiler require a hack defined by _DMAIN_COMP
             # (see examples/c_example.c)
